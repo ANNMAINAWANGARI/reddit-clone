e4c206437f7b7a0bcb733200f7d55b8f0cfde68e
ANNMAINAWANGARI <wangariann371@gmail.com>
2022-12-21 14:51:28 +0300

    chakra theme setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import {theme} from '../chakra/theme'
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
