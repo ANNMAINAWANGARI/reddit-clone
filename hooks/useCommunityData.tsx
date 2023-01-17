03da667c9c05b7f21d19d10109e8b05f72003e32
ANNMAINAWANGARI <wangariann371@gmail.com>
2023-01-17 23:31:55 +0300

    global communities state

---
+++ hooks/useCommunityData.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type useCommunityDataProps = {
+    
+};
+
+const useCommunityData:React.FC<useCommunityDataProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default useCommunityData;
