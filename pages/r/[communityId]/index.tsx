3044648bd28da5bed65527e7018fff4ddd4c95fd
ANNMAINAWANGARI <wangariann371@gmail.com>
2023-01-31 00:58:42 +0300

    Community about component

--- pages/r/[communityId]/index.tsx
+++ pages/r/[communityId]/index.tsx
@@ -1,35 +1,44 @@
 import { doc, getDoc } from 'firebase/firestore';
 import { GetServerSidePropsContext } from 'next';
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import { firestore } from '../../../firebase/clientApp';
-import { Community } from '../../../state/atoms/CommunitiesAtom';
+import { Community, communityState } from '../../../state/atoms/CommunitiesAtom';
 import safeJsonStringify from 'safe-json-stringify'
 import NotFound from '../../../components/Community/NotFound';
 import Header from '../../../components/Community/Header';
 import PageContent from '../../../components/Layout/PageContent';
 import CreatePostLink from '../../../components/Community/CreatePostLink';
 import Posts from '../../../components/Posts/Posts';
+import { useSetRecoilState } from 'recoil';
+import About from '../../../components/Community/About';
 
 type CommunityPageProps = {
     communityData:Community
 };
 
 const CommunityPage:React.FC<CommunityPageProps> = ({communityData}) => {
+    const setCommunityStateValue = useSetRecoilState(communityState)
+    const [loading,setLoading] = useState(false)
+    useEffect(()=>{
+        setCommunityStateValue((prev)=>({
+            ...prev,
+            currentCommunity:communityData
+        }))
+      },[])
     if (!communityData) {
-        return (
-         <NotFound/>
-        )
-      }
-      console.log(communityData)
+        return <NotFound/>}
+      
+    //   console.log(communityData)
+      
     return (
         <>
         <Header communityData={communityData}/>
         <PageContent>
             <>
              <CreatePostLink/>
-             <Posts communityData={communityData }/>
+             <Posts communityData={communityData}/>
             </>
-            <>RHS</>
+            <><About communityData={communityData} loading={loading}/></>
         </PageContent>
         </>
     )
--- pages/r/[communityId]/submit.tsx
+++ pages/r/[communityId]/submit.tsx
@@ -1,9 +1,11 @@
 import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import PageContent from '../../../components/Layout/PageContent';
 import NewPostForm from '../../../components/Posts/NewPostForm';
 import { auth } from '../../../firebase/clientApp';
+import { communityState } from '../../../state/atoms/CommunitiesAtom';
 
 type submitProps = {
     
@@ -11,6 +13,8 @@ type submitProps = {
 
 const SubmitPostPage:React.FC<submitProps> = () => {
     const [user] = useAuthState(auth)
+    const communityStateValue = useRecoilValue(communityState)
+    console.log('communityStateValue',communityStateValue)
     return(
         <PageContent>
             <>
